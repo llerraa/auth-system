8082515001c8dfc74ce5219a371d520f3cb4fccc
Valeria Perevozchikova <valeria_svoyak@MacBook-Pro-Valeria.local>
2026-04-03 11:27:02 +0300

    Add init.sql for auto-create users table

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
